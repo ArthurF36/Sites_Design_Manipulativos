da3cb700a6b2538e72692b7bd2d37bf1049df790
Arthur Franco <101573046+ArthurF36@users.noreply.github.com>
2026-05-03 17:59:52 +0000

    Update salvar_Df.py

--- Tratar_dados/Menu.py
+++ Tratar_dados/Menu.py
@@ -11,7 +11,7 @@ def Menu(configs):
         try:
             # Tenta converter a entrada para número inteiro
             opcao = int(entrada)
-            
+
             if opcao == 1:
                 print("🤖 Modo Gemini ativado.")
                 return configs["gemini"], "Gemini"
@@ -21,7 +21,7 @@ def Menu(configs):
             else:
                 # Se for número, mas não for 1 ou 2
                 print("❌ Erro: Opção inválida! Escolha apenas 1 ou 2.")
-                
+      
         except ValueError:
             # Se o usuário digitar letras, símbolos ou deixar vazio
             print(f"❌ Erro: '{entrada}' não é um número válido. Digite 1 ou 2.")
